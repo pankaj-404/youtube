bb07549e22761a0034ea53028587e6ac022828ae
pankaj-404 <pankajbhayre@gmial.com>
2023-12-13 17:11:32 +0530

    Make util functions

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+    (err, req, res, next) => {
+        Promise
+            .resolve(requestHandler(err, req, res, next))
+            .catch((error => next(error)))
+    }
+}
+
+const asyncTyChHandler = (fn) => async (err, req, res, next) => {
+    try {
+        await fn(err, req, res, next)
+    } catch (error) {
+        res.status(err.code || error.code || 500).json({
+            success: false,
+            message: err.message || error.message || "Server error"
+        })
+    }
+}
+
+export { asyncHandler, asyncTyChHandler }
