a8006af7255ceacf266febd4545af3611d667a38
pankaj-404 <pankajbhayre@gmial.com>
2023-12-29 00:53:37 +0530

    add access token and refresh token options

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,15 @@
 export const DB_NAME = "youtube"
 export const receivingDataLimit = "16kb"
+export const accessTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 1 * 60 * 60 * 1000,
+}
+
+export const refreshTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+}
